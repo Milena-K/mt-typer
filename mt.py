ade35ef4b94516ef64cbeb1a4cc5838bcc41fd3d
milena.kukolj <mkukolj@protonmail.com>
2020-09-18 13:40:53 +0200

    First sort of working version

---
+++ mt.py
@@ -0,0 +1,22 @@
+# open up chrome
+# type https://monkey-type.com/
+# make a screenshot of the text
+# save it as '1.jpg' to the image path below
+
+# read text from screen (tesseract ocr)
+from tesserocr import PyTessBaseAPI
+
+image = 'C:\\Users\\Milena\\Documents\\Programming\\Python\\mt-typer\\pictures\\1.jpg'
+DATA_PATH = 'C:\\Users\\Milena\\Documents\\Programming\\Python\\mt-typer\\tesserocr-master\\tessdata'
+
+with PyTessBaseAPI(DATA_PATH) as api:
+	api.SetImageFile(image)
+	text = api.GetUTF8Text()
+	print(text)
+
+# write text in the field  
+import keyboard
+
+for word in text.split():
+	keyboard.wait('right arrow')
+	keyboard.write(word + " ")
